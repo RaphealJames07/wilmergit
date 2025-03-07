e1348a546734f55129947d9e122020b2565708d6
Benjamin <benjaminufuomajacob9@gmail.com>
2025-03-07 16:01:50 +0100

    touch homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,12 @@ const Home = () => {
     <div>
       Home page
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis,
-        suscipit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A repellat
+        voluptas fugiat quidem necessitatibus, facere accusamus saepe error sed
+        autem aspernatur dolorum rem eligendi ducimus sunt delectus quisquam
+        assumenda itaque quis libero mollitia ad vero eius. Distinctio ipsa
+        itaque iusto blanditiis aperiam impedit quae saepe aliquid debitis.
+        Mollitia, soluta dolores?
       </p>
     </div>
   );
