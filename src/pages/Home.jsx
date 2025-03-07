d2327e50e826496f8302bc02fba3ece0acc68ca0
Benjamin <benjaminufuomajacob9@gmail.com>
2025-03-07 15:54:41 +0100

    new changes to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis,
+        suscipit.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
