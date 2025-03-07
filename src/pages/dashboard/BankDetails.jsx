f8ce2f7c9499700c82632616791b7cb00c40b6ba
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2025-03-07 15:53:00 +0100

    finished my work

--- src/pages/dashboard/BankDetails.jsx
+++ src/pages/dashboard/BankDetails.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const BankDetails = () => {
   return (
-    <div>BankDetails</div>
+    <div>
+      <p>Bank Details</p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias incidunt vero a quos laborum expedita sint vitae fugit, quo itaque minima beatae ipsa labore mollitia explicabo, eos harum odio. Obcaecati itaque totam omnis aliquam harum facilis impedit asperiores. Dolorem, at?
+    </div>
   )
 }
 
