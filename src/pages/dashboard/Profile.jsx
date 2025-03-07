11f64d837c7c4de08363d1d06adba81c22d26ba3
Benjamin <benjaminufuomajacob9@gmail.com>
2025-03-07 14:28:05 +0100

    finish profile page

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic soluta
+      explicabo placeat deleniti similique obcaecati, ipsam minus ex impedit
+      nisi voluptate velit! Eius, quasi. Quisquam minus, distinctio odio
+      incidunt ullam quasi non beatae voluptatem amet, eum hic fugit itaque
+      delectus?
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
