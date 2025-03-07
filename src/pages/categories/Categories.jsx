5d2c7a7852ca5d4734b18e6a8b1cdfe1621ea2b1
ogechiedithodo <ogechiedithodo@gmail.com>
2025-03-07 16:39:30 +0100

    updated category

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Categories = () => {
   return (
     <div><p>
-      Lorem ipsum dolor sit amet consectetur,
-       adipisicing elit. Animi distinctio incidunt expedita culpa voluptates cum quos,
-        quidem quas provident, molestiae ipsa sed,
-         vitae voluptatem asperiores adipisci placeat doloribus!
-          Iure quos assumenda consequatur corrupti dicta saepe aliquam mollitia nisi ipsum pariatur?
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Optio odit soluta, tempora, blanditiis mollitia reiciendis numquam, 
+      amet vel ipsum magni quo facere. Recusandae labore beatae dolores animi ex nesciunt 
+      veritatis asperiores alias aperiam 
+     repellat officiis sequi hic a, officia repudiandae!
           </p></div>
   )
 }
